9fdedd36fb69c49b22208c860d4a9f528b698d1c
YasminMiranda03 <3946136940101@ingenieria.usac.edu.gt>
2025-12-09 00:19:05 -0600

    clase nodo y lista simple

---
+++ src/estructuras/ListaSimple.py
@@ -0,0 +1,42 @@
+from estructuras.Nodo import Nodo       
+class ListaSimple:
+    def __init__(self): 
+        self.cabeza = None 
+    
+    def agregar(self, dato): 
+        nuevo_nodo = Nodo(dato):
+        if not self.cabeza:     #verifica si la lista esta vacia
+            self.cabeza = nuevo_nodo 
+        else: 
+            actual = self.cabeza 
+            while actual.siguiente:     #recorre la lista hasta encontrar el ultimo nodo
+                actual = actual.siguiente 
+            actual.siguiente = nuevo_nodo
+    
+    def buscar(self, id):   #busca un nodo por id
+        actual = self.cabeza
+        while actual:
+            if actual.id == :
+                return True
+            actual = actual.siguiente 
+    
+    def eliminar(self, id):  #elimina un nodo por id
+        actual = self.cabeza
+        anterior = None
+        while actual:
+            if actual.id == id:
+                if anterior:    
+                    anterior.siguiente = actual.siguiente
+                else:
+                    self.cabeza = actual.siguiente
+                return True
+            anterior = actual
+            actual = actual.siguiente
+        return False
+    
+    def recorrer(self):   #recorre la lista y devuelve una lista con los datos
+        actual = self.cabeza
+        while actual:
+            yield actual.dato
+            actual = actual.siguiente
+    
